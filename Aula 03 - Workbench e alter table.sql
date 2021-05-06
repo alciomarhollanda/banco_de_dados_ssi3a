b5eb626fa95208d3effd21b3e419bdad010f7b98
Alciomar Hollanda <alciomarhollanda@gmail.com>
2021-05-05 22:08:06 -0300

    Aula 03 -  Alter Table Add, drop, modify and change

---
+++ Aula 03 - Workbench e alter table.sql
@@ -0,0 +1,19 @@
+create table aluno(
+	idAluno int primary key auto_increment,
+    nomeAluno varchar(50)
+);
+
+alter table aluno add idadeAluno int;
+
+alter table aluno drop column idadeAluno;
+
+alter table aluno modify column nomeAluno varchar(80) not null;
+
+alter table aluno change column nomeAluno nome_aluno varchar(30);
+
+
+
+
+
+
+
